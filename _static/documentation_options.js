77d3e9bf11cd7d0c434103c079a297a5781c0809
Romain Thomas <me@romainthomas.fr>
2022-11-01 08:01:01 +0100

    Add doc about completion

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0-9e4e958',
+    VERSION: '1.0.1-86087c7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
