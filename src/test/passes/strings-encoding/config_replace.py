f87123bb341fd7f0e7a45327746a3d9a5a82b2cc
Stefan Gränitz <stefan.graenitz@gmail.com>
2023-05-24 10:41:32 +0200

    Basic test for string-encoding configs
    In order to inspect individual tests, first dump the test command line:
    ```
    > cd /path/to/o-mvll-build-ndk-r25c
    > ninja -t commands check | tail -1
    cd /path/to/o-mvll-build-ndk-r25c/test && /usr/local/bin/python3.8 /path/to/llvm-toolchain-android/out/stage2/./bin/llvm-lit -vv --no-progress-bar --show-xfail --show-unsupported /path/to/o-mvll-build-ndk-r25c/test
    ```
    
    Then rerun llvm-lit manually. The `--filter` option selects a subset of tests. The `-a` option dumps all executed commands:
    ```
    > python3.8 /path/to/llvm-toolchain-android/out/stage2/bin/llvm-lit -a --filter="strings-encoding/basic" test
    Running tests with: /path/to/llvm-toolchain-android/out_r25c/stage2/bin/clang
    Testing plugin file: /path/to/o-mvll-build-ndk-r25c/libOMVLL.so
    OMVLL_PYTHONPATH: /path/to/o-mvll-build-ndk-r25c/../Python-3.10.7/Lib
    -- Testing: 1 of 4 tests, 1 workers --
    PASS: O-MVLL Tests :: passes/strings-encoding/basic-aarch64-android.cpp (1 of 1)
    Script:
    --
    : 'RUN: at line 4';   /path/to/llvm-toolchain-android/out_r25c/stage2/bin/clang++ -target aarch64-linux-android -O1 -c /path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp -o - | strings | /path/to/llvm-toolchain-android/out_r25c/stage2/bin/FileCheck --check-prefix=CHECK-DEFAULT -DFILE_NAME=/path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp /path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp
    : 'RUN: at line 8';   env OMVLL_CONFIG=/path/to/o-mvll/src/test/passes/strings-encoding/config_remove.py /path/to/llvm-toolchain-android/out_r25c/stage2/bin/clang++ -target aarch64-linux-android -fpass-plugin=/path/to/o-mvll-build-ndk-r25c/libOMVLL.so -O1 -c /path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp                                               -o - | strings | /path/to/llvm-toolchain-android/out_r25c/stage2/bin/FileCheck --check-prefix=CHECK-REMOVED -DFILE_NAME=/path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp /path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp
    : 'RUN: at line 14';   env OMVLL_CONFIG=/path/to/o-mvll/src/test/passes/strings-encoding/config_replace.py /path/to/llvm-toolchain-android/out_r25c/stage2/bin/clang++ -target aarch64-linux-android -fpass-plugin=/path/to/o-mvll-build-ndk-r25c/libOMVLL.so -O1 -c /path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp                                                -o - | strings | /path/to/llvm-toolchain-android/out_r25c/stage2/bin/FileCheck --check-prefix=CHECK-REPLACED -DFILE_NAME=/path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp /path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp
    --
    Exit Code: 0
    
    ********************
    
    Testing Time: 0.31s
      Excluded: 3
      Passed  : 1
    ```
    
    Copy out one of the RUN lines to inspect the actual output that llvm-lit feeds into FileCheck:
    ```
    > env OMVLL_CONFIG=/path/to/o-mvll/src/test/passes/strings-encoding/config_replace.py /path/to/llvm-toolchain-android/out_r25c/stage2/bin/clang++ -target aarch64-linux-android -fpass-plugin=/path/to/o-mvll-build-ndk-r25c/libOMVLL.so -O1 -c /path/to/o-mvll/src/test/passes/strings-encoding/basic-aarch64-android.cpp -o - | strings
    ,ij8k
    q+i*8k
    Error: %s (%s:%d)
    Wrong input
    Android (dev, based on r450784d1) clang version 14.0.7 (https://android.googlesource.com/toolchain/llvm-project 4c603efb0cca074e9238af8b4106c30add4418f6)
    _Z7fprintfPvPKcz
    .rela.text
    .comment
    .bss
    stderr
    basic-aarch64-android.cpp
    .note.GNU-stack
    _Z10check_codei
    .rela.eh_frame
    .strtab
    .symtab
    .data
    $d.6
    $d.5
    $d.4
    $d.3
    $d.2
    $d.1
    .rodata.str1.1
    .rela.init_array.0
    $x.0
    ```

---
+++ src/test/passes/strings-encoding/config_replace.py
@@ -0,0 +1,13 @@
+import omvll
+from functools import lru_cache
+
+class MyConfig(omvll.ObfuscationConfig):
+    def __init__(self):
+        super().__init__()
+    def obfuscate_string(self, _, __, string: bytes):
+        if string.endswith(b".cpp"):
+            return omvll.StringEncOptGlobal()
+
+@lru_cache(maxsize=1)
+def omvll_get_config() -> omvll.ObfuscationConfig:
+    return MyConfig()
