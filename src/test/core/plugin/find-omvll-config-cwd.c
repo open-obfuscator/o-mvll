70177c7e5026bfc8d8513391a2bc4bf2d8c46d59
Stefan Gränitz <stefan.graenitz@gmail.com>
2023-06-01 15:24:54 +0200

    Add test for status quo: find omvll.yml in CWD

---
+++ src/test/core/plugin/find-omvll-config-cwd.c
@@ -0,0 +1,16 @@
+// Prepare test output dir with yml-config, py-config and fake python-libs dir
+// RUN: rm -rf %T_cwd
+// RUN: mkdir -p %T_cwd
+// RUN: cp %S/Inputs/omvll.yml %T_cwd/omvll.yml
+// RUN: cp %S/Inputs/config_empty.py %T_cwd/config_empty.py
+
+// Run clang from the test output dir
+// RUN: cd %T_cwd
+// RUN: clang -target aarch64-linux-android -fno-legacy-pass-manager -fpass-plugin=%libOMVLL -S %s -o /dev/null
+// RUN: FileCheck %s -DCWD=%T_cwd < omvll.log
+
+// Check that we load the yml-config from the test output dir
+// CHECK: Looking for omvll.yml in [[CWD]]
+// CHECK: Loading omvll.yml from [[CWD]]/omvll.yml
+
+void test() {}
