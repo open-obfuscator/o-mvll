c57555c2270d000f29c405ae2f59680e7a5e4f7c
Sergi Sevil <sergi.sevil@build38.com>
2024-08-01 15:05:17 +0200

    Fix CI pipeline, add `xcode_14_compile_macos.sh`

--- scripts/docker/xcode_15_compile_macos.sh
+++ scripts/docker/xcode_15_compile_macos.sh
@@ -4,14 +4,14 @@ mkdir -p deps && cd deps
 
 export DEPS_FOLDER=$(pwd)
 
-cp ../tmp/third-party-xcode14/omvll-deps-xcode-*/LLVM-14.0.0git-arm64-Darwin.tar.gz .
-cp ../tmp/third-party-xcode14/omvll-deps-xcode-*/LLVM-14.0.0git-x86_64-Darwin.tar.gz .
-cp ../tmp/third-party-xcode14/omvll-deps-xcode-*/Python-slim.tar.gz .
-cp ../tmp/third-party-xcode14/omvll-deps-xcode-*/pybind11.tar.gz .
-cp ../tmp/third-party-xcode14/omvll-deps-xcode-*/spdlog-1.10.0-Darwin.tar.gz .
+cp ../tmp/third-party-xcode15/omvll-deps-xcode-*/LLVM-16.0.0git-arm64-Darwin.tar.gz .
+cp ../tmp/third-party-xcode15/omvll-deps-xcode-*/LLVM-16.0.0git-x86_64-Darwin.tar.gz .
+cp ../tmp/third-party-xcode15/omvll-deps-xcode-*/Python-slim.tar.gz .
+cp ../tmp/third-party-xcode15/omvll-deps-xcode-*/pybind11.tar.gz .
+cp ../tmp/third-party-xcode15/omvll-deps-xcode-*/spdlog-1.10.0-Darwin.tar.gz .
 
-tar xzvf LLVM-14.0.0git-arm64-Darwin.tar.gz
-tar xzvf LLVM-14.0.0git-x86_64-Darwin.tar.gz
+tar xzvf LLVM-16.0.0git-arm64-Darwin.tar.gz
+tar xzvf LLVM-16.0.0git-x86_64-Darwin.tar.gz
 tar xzvf Python-slim.tar.gz
 tar xzvf pybind11.tar.gz
 tar xzvf spdlog-1.10.0-Darwin.tar.gz
@@ -26,9 +26,9 @@ mkdir -p arm64 && cd arm64
 
 cmake -GNinja ../.. \
       -DCMAKE_OSX_ARCHITECTURES="arm64" \
-      -DCMAKE_OSX_DEPLOYMENT_TARGET="13.0" \
-      -DCMAKE_CXX_COMPILER=${DEPS_FOLDER}/LLVM-14.0.0git-arm64-Darwin/bin/clang++ \
-      -DCMAKE_C_COMPILER=${DEPS_FOLDER}/LLVM-14.0.0git-arm64-Darwin/bin/clang \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET="14.5" \
+      -DCMAKE_CXX_COMPILER=${DEPS_FOLDER}/LLVM-16.0.0git-arm64-Darwin/bin/clang++ \
+      -DCMAKE_C_COMPILER=${DEPS_FOLDER}/LLVM-16.0.0git-arm64-Darwin/bin/clang \
       -DCMAKE_BUILD_TYPE=Release \
       -DPYBIND11_NOPYTHON=1 \
       -DPython3_ROOT_DIR=${DEPS_FOLDER} \
@@ -36,7 +36,7 @@ cmake -GNinja ../.. \
       -DPython3_INCLUDE_DIR=${DEPS_FOLDER}/include/python3.10 \
       -Dpybind11_DIR=${DEPS_FOLDER}/share/cmake/pybind11 \
       -Dspdlog_DIR=${DEPS_FOLDER}/lib/cmake/spdlog \
-      -DLLVM_DIR=${DEPS_FOLDER}/LLVM-14.0.0git-arm64-Darwin/lib/cmake/llvm \
+      -DLLVM_DIR=${DEPS_FOLDER}/LLVM-16.0.0git-arm64-Darwin/lib/cmake/llvm \
       -DLLVM_TOOLS_DIR=${OMVLL_LLVM_TOOLS_DIR} \
       -DLLVM_EXTERNAL_LIT=${OMVLL_LLVM_TOOLS_DIR}/bin/llvm-lit
 
@@ -47,9 +47,9 @@ mkdir -p x86_64 && cd x86_64
 
 cmake -GNinja ../.. \
       -DCMAKE_OSX_ARCHITECTURES="x86_64" \
-      -DCMAKE_OSX_DEPLOYMENT_TARGET="13.0" \
-      -DCMAKE_CXX_COMPILER=${DEPS_FOLDER}/LLVM-14.0.0git-x86_64-Darwin/bin/clang++ \
-      -DCMAKE_C_COMPILER=${DEPS_FOLDER}/LLVM-14.0.0git-x86_64-Darwin/bin/clang \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET="14.5" \
+      -DCMAKE_CXX_COMPILER=${DEPS_FOLDER}/LLVM-16.0.0git-x86_64-Darwin/bin/clang++ \
+      -DCMAKE_C_COMPILER=${DEPS_FOLDER}/LLVM-16.0.0git-x86_64-Darwin/bin/clang \
       -DCMAKE_BUILD_TYPE=Release \
       -DPYBIND11_NOPYTHON=1 \
       -DPython3_ROOT_DIR=${DEPS_FOLDER} \
@@ -57,7 +57,7 @@ cmake -GNinja ../.. \
       -DPython3_INCLUDE_DIR=${DEPS_FOLDER}/include/python3.10 \
       -Dpybind11_DIR=${DEPS_FOLDER}/share/cmake/pybind11 \
       -Dspdlog_DIR=${DEPS_FOLDER}/lib/cmake/spdlog \
-      -DLLVM_DIR=${DEPS_FOLDER}/LLVM-14.0.0git-x86_64-Darwin/lib/cmake/llvm \
+      -DLLVM_DIR=${DEPS_FOLDER}/LLVM-16.0.0git-x86_64-Darwin/lib/cmake/llvm \
       -DLLVM_TOOLS_DIR=${OMVLL_LLVM_TOOLS_DIR} \
       -DLLVM_EXTERNAL_LIT=${OMVLL_LLVM_TOOLS_DIR}/bin/llvm-lit
 
