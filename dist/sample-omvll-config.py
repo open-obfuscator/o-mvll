269c542b7d105df5aae69e5f4c3a3f22135f773f
Sergi Sevil <sergi.sevil@build38.com>
2025-10-01 11:23:50 +0200

    Add new passes inside sample config

--- dist/sample-omvll-config.py
+++ dist/sample-omvll-config.py
@@ -15,6 +15,18 @@ class MyConfig(omvll.ObfuscationConfig):
     def obfuscate_string(self, _, __, string: bytes):
         return omvll.StringEncOptGlobal()
 
+    def indirect_call(self, mod: omvll.Module, func: omvll.Function):
+        return omvll.ObfuscationConfig.default_config(self, mod, func, [], [], [], 10)
+
+    def break_control_flow(self, mod: omvll.Module, func: omvll.Function):
+        return omvll.ObfuscationConfig.default_config(self, mod, func, [], [], [], 10)
+
+    def function_outline(self, _, __):
+        return omvll.FunctionOutlineWithProbability(10)
+
+    def basic_block_duplicate(self, _, __):
+        return omvll.BasicBlockDuplicateWithProbability(10)
+
 @lru_cache(maxsize=1)
 def omvll_get_config() -> omvll.ObfuscationConfig:
     """
